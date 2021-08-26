c1df69e15a21de1d4f922630d8ff2b747cae7c8d
Lsong <song940@gmail.com>
2021-08-26 08:08:10 -0400

    update

--- funkit-api/server.js
+++ funkit-api/server.js
@@ -3,11 +3,12 @@ const kelp = require('kelp');
 const body = require('kelp-body');
 const send = require('kelp-send');
 const Router = require('kelp-router');
-const { MongoClient } = require('mongodb');
+const logger = require('kelp-logger');
+const { MongoClient, ObjectId } = require('mongodb');
 
 
 (async () => {
-  const client = new MongoClient('mongodb://lsong.me:27017');
+  const client = new MongoClient('mongodb://localhost:27017');
   console.log('mongodb is connecting ...');
   await client.connect();
   console.log('mongodb is connected.');
@@ -21,32 +22,46 @@ const { MongoClient } = require('mongodb');
   const app = kelp();
   app.use(body);
   app.use(send);
+  app.use(logger);
   app.use(router);
   // list
   router.get('/workers', async (req, res) => {
-    const workers = await Worker.find();
+    const workers = await Worker.find().toArray();
     res.send({ workers });
   });
-  // get
-  router.get('/worker/:id', async (req, res) => {
-    const { id } = req.params;
-    const worker = await Worker.findOne({ id });
-    res.send({ worker });
+  router.options('/workers', (req, res) => {
+    res.writeHead(200, {
+      // 'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+    });
+    res.end();
   });
-  
   // create or update
-  router.post('/worker', async (req, res) => {
+  router.post('/workers', async (req, res) => {
     const { id, name, code } = req.body;
-    const query = { id };
-    const update = { $set: { name, code } };
-    const options = { upsert: true };
-    const worker = await Worker.updateOne(query, update, options);
+    if (id) {
+      console.log('update');
+      const update = { $set: { name, code } };
+      const _id = ObjectId(id);
+      const result = await Worker.updateOne({ _id }, update);
+      res.send({ id, success: result.acknowledged });
+    } else {
+      const result = await Worker.insertOne({ name, code });
+      res.send({ id: result.insertedId, success: result.acknowledged });
+    }
+  });
+  // get
+  router.get('/workers/:id', async (req, res) => {
+    const { id } = req.params;
+    const _id = ObjectId(id);
+    const worker = await Worker.findOne({ _id });
     res.send({ worker });
   });
 
   app.use((req, res) => res.send(404));
   const server = http.createServer(app);
-  server.listen(3000, () => {
+  server.listen(3005, () => {
     console.log('funkit api server is running at 3000');
   });
 
